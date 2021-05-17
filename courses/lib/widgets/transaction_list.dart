d9ef9c4c402542ba673a21a7ef7be4618fef8dc7
Moussa Sow <sowmoussa@MacBook-Pro-de-Moussa.local>
2021-05-17 17:47:18 +0200

    last commit

--- courses/lib/widgets/transaction_list.dart
+++ courses/lib/widgets/transaction_list.dart
@@ -11,7 +11,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
+      height: 600,
       child: _userTransaction.isEmpty
           ? Column(
               children: [
