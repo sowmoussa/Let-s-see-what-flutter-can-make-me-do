ab03527c35d7ccb6f57e4808a5b626e9822caed0
Moussa Sow <sowmoussa@MacBook-Pro-de-Moussa.local>
2021-05-25 00:09:47 +0200

    routing first

---
+++ recipes/lib/categories_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:recipes/category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals'),
+      ),
+      body: GridView(
+        padding: EdgeInsets.all(10),
+        children: DUMMY_CATEGORIES
+            .map(
+              (e) => CategoryItem(
+                e.id,
+                e.title,
+                e.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ recipes/lib/main.dart
@@ -0,0 +1,42 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
+import './categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals APP',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            bodyText1: TextStyle(
+              color: Color.fromRGBO(20, 51, 21, 1),
+            ),
+            bodyText2: TextStyle(
+              color: Color.fromRGBO(20, 51, 21, 1),
+            ),
+            headline6: TextStyle(
+              fontSize: 24,
+              fontFamily: 'RobotoCondensed',
+            )),
+      ),
+      //home: CategoriesScreen(),
+      initialRoute: '/',
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        '/category-meals': (ctx) => CategoryMealsScreen(),
+      },
+    );
+  }
+}
