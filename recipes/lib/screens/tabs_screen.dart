506fad569dfffdaac1de8ddfb8c5f50f0f636d94
Moussa Sow <sowmoussa@MacBook-Pro-de-Moussa.local>
2021-05-25 21:45:11 +0200

    Added bottom tabBar

--- recipes/lib/screens/tabs_screen.dart
+++ recipes/lib/screens/tabs_screen.dart
@@ -10,32 +10,44 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'pages': CategoriesScreen(), 'title': 'Meals'},
+    {'pages': FavouritesScreen(), 'title': 'Favourites'},
+  ];
+  int _selectedIndexPage = 0;
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedIndexPage = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favourites',
-              )
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedIndexPage]['title']),
+      ),
+      body: _pages[_selectedIndexPage]['pages'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectedPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedIndexPage,
+        // type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: "Categories",
           ),
-        ),
-        body: TabBarView(
-          children: [
-            CategoriesScreen(),
-            FavouritesScreen(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: "Favourites",
+          ),
+        ],
       ),
     );
   }
